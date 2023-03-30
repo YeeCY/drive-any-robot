fix bugs: loss computation and optimizer update orders; stop gradients for img encoder from actor loss

train/stable_contrastive_rl_train/models/networks.py
@@ -187,18 +187,18 @@ class ContrastivePolicy(nn.Module):
         self.dist_mu_layers = nn.Sequential(
             nn.Linear(32, 1),
         )
-        # self.waypoint_log_std_layers = nn.Sequential(
-        #     nn.Linear(32, self.action_size - 1),
-        #     nn.Sigmoid()
-        # )
-        # self.dist_log_std_layers = nn.Sequential(
-        #     nn.Linear(32, 1),
-        #     nn.Sigmoid()
-        # )
-        self.waypoint_log_std_logits = nn.Parameter(
-            torch.zeros(self.action_size - 1, requires_grad=True))
-        self.dist_log_std_logits = nn.Parameter(
-            torch.zeros(1, requires_grad=True))
+        self.waypoint_log_std_layers = nn.Sequential(
+            nn.Linear(32, self.action_size - 1),
+            nn.Sigmoid()
+        )
+        self.dist_log_std_layers = nn.Sequential(
+            nn.Linear(32, 1),
+            nn.Sigmoid()
+        )
+        # self.waypoint_log_std_logits = nn.Parameter(
+        #     torch.zeros(self.action_size - 1, requires_grad=True))
+        # self.dist_log_std_logits = nn.Parameter(
+        #     torch.zeros(1, requires_grad=True))
 
     def forward(
         self, obs_img: torch.tensor, goal_img: torch.tensor, detach_img_encode: bool = False,
@@ -206,19 +206,19 @@ class ContrastivePolicy(nn.Module):
         obs_encoding, goal_encoding = self.img_encoder(obs_img, goal_img)
 
         # DELETEME (chongyiz)
-        # if detach_img_encode:
-        #     obs_encoding = obs_encoding.detach()
-        #     goal_encoding = goal_encoding.detach()
+        if detach_img_encode:
+            obs_encoding = obs_encoding.detach()
+            goal_encoding = goal_encoding.detach()
 
         obs_goal_encoding = self.linear_layers(
             torch.cat([obs_encoding, goal_encoding], dim=-1))
 
         waypoint_mu = self.waypoint_mu_layers(obs_goal_encoding)
         dist_mu = self.dist_mu_layers(obs_goal_encoding)
-        # waypoint_log_std = self.waypoint_log_std_layers(obs_goal_encoding)
-        # dist_log_std = self.dist_log_std_layers(obs_goal_encoding)
-        waypoint_log_std = torch.sigmoid(self.waypoint_log_std_logits)
-        dist_log_std = torch.sigmoid(self.dist_log_std_logits)
+        waypoint_log_std = self.waypoint_log_std_layers(obs_goal_encoding)
+        dist_log_std = self.dist_log_std_layers(obs_goal_encoding)
+        # waypoint_log_std = torch.sigmoid(self.waypoint_log_std_logits)
+        # dist_log_std = torch.sigmoid(self.dist_log_std_logits)
         waypoint_log_std = self.min_log_std + waypoint_log_std * (
                 self.max_log_std - self.min_log_std)
         dist_log_std = self.min_log_std + dist_log_std * (
@@ -241,36 +241,37 @@ class ContrastivePolicy(nn.Module):
             (waypoint_mu.shape[0], self.action_size - 1))
 
         mu = torch.cat([waypoint_mu, dist_mu], dim=-1)
-        std = torch.cat([waypoint_std, dist_std], dim=-1).unsqueeze(0).repeat(
-            mu.shape[0], 1)
+        std = torch.cat([waypoint_std, dist_std], dim=-1)
+        # std = torch.cat([waypoint_std, dist_std], dim=-1).unsqueeze(0).repeat(
+        #     mu.shape[0], 1)
 
         return mu, std
 
-    # def parameters(self, recurse: bool = True) -> Iterator[nn.Parameter]:
-    #     for name, param in chain(self.linear_layers.named_parameters(recurse=recurse),
-    #                              self.waypoint_mu_layers.named_parameters(recurse=recurse),
-    #                              self.waypoint_log_std_layers.named_parameters(recurse=recurse),
-    #                              self.dist_mu_layers.named_parameters(recurse=recurse),
-    #                              self.dist_log_std_layers.named_parameters(recurse=recurse)):
-    #         yield param
-    #
-    # def named_parameters(self, prefix: str = '', recurse: bool = True) -> Iterator[Tuple[str, nn.Parameter]]:
-    #     gen = chain(
-    #         self.linear_layers._named_members(
-    #             lambda module: module._parameters.items(),
-    #             prefix=prefix, recurse=recurse),
-    #         self.waypoint_mu_layers._named_members(
-    #             lambda module: module._parameters.items(),
-    #             prefix=prefix, recurse=recurse),
-    #         self.waypoint_log_std_layers._named_members(
-    #             lambda module: module._parameters.items(),
-    #             prefix=prefix, recurse=recurse),
-    #         self.dist_mu_layers._named_members(
-    #             lambda module: module._parameters.items(),
-    #             prefix=prefix, recurse=recurse),
-    #         self.dist_log_std_layers._named_members(
-    #             lambda module: module._parameters.items(),
-    #             prefix=prefix, recurse=recurse),
-    #     )
-    #     for elem in gen:
-    #         yield elem
+    def parameters(self, recurse: bool = True) -> Iterator[nn.Parameter]:
+        for name, param in chain(self.linear_layers.named_parameters(recurse=recurse),
+                                 self.waypoint_mu_layers.named_parameters(recurse=recurse),
+                                 self.waypoint_log_std_layers.named_parameters(recurse=recurse),
+                                 self.dist_mu_layers.named_parameters(recurse=recurse),
+                                 self.dist_log_std_layers.named_parameters(recurse=recurse)):
+            yield param
+
+    def named_parameters(self, prefix: str = '', recurse: bool = True) -> Iterator[Tuple[str, nn.Parameter]]:
+        gen = chain(
+            self.linear_layers._named_members(
+                lambda module: module._parameters.items(),
+                prefix=prefix, recurse=recurse),
+            self.waypoint_mu_layers._named_members(
+                lambda module: module._parameters.items(),
+                prefix=prefix, recurse=recurse),
+            self.waypoint_log_std_layers._named_members(
+                lambda module: module._parameters.items(),
+                prefix=prefix, recurse=recurse),
+            self.dist_mu_layers._named_members(
+                lambda module: module._parameters.items(),
+                prefix=prefix, recurse=recurse),
+            self.dist_log_std_layers._named_members(
+                lambda module: module._parameters.items(),
+                prefix=prefix, recurse=recurse),
+        )
+        for elem in gen:
+            yield elem

train/stable_contrastive_rl_train/training/train_utils.py
@@ -361,13 +361,26 @@ def train(
         # _, action_pred = model(action_obs_data, action_goal_data)
         # action_loss = F.mse_loss(action_pred, action_label)
 
+        # Important: the order of loss computation and optimizer update matter!
+        # compute critic loss
         critic_loss = get_critic_loss(
             model, obs_data, next_obs_data, action_data, goal_data,
             discount, use_td=use_td)
 
+        # optimize critic
+        optimizer["critic_optimizer"].zero_grad()
+        critic_loss.backward()
+        optimizer["critic_optimizer"].step()
+
+        # compute actor loss
         actor_loss, actor_q_loss, gcbc_loss, waypoint_gcbc_loss, dist_gcbc_loss = get_actor_loss(
             model, obs_data, action_data, goal_data,
             bc_coef=bc_coef, stop_grad_actor_img_encoder=stop_grad_actor_img_encoder)
+
+        # optimize actor
+        optimizer["actor_optimizer"].zero_grad()
+        actor_loss.backward()
+        optimizer["actor_optimizer"].step()
 
         # preds = model.policy_network(obs_data, goal_data).mean
         # action_data are not used here
@@ -415,9 +428,9 @@ def train(
         # torch.nn.utils.clip_grad_norm(
         #     model.q_network.parameters(), 1.0)
         # optimizer["critic_optimizer"].step()
-        optimizer['optimizer'].zero_grad()
-        (actor_loss + critic_loss).backward()
-        optimizer['optimizer'].step()
+        # optimizer['optimizer'].zero_grad()
+        # (actor_loss + critic_loss).backward()
+        # optimizer['optimizer'].step()
 
         if i % target_update_freq == 0:
             model.soft_update_target_q_network()
@@ -880,7 +893,7 @@ def get_actor_loss(model, obs, orig_action, goal, bc_coef=0.05,
     log_prob = dist.log_prob(sampled_action)
 
     # q_action = model.q_network(obs, sampled_action, goal)
-    obs_a_repr, g_repr, _, _, _, _ = model(obs, sampled_action, goal)
+    obs_a_repr, g_repr, _, _, _, _ = model(obs, mean, goal)
     q_action = torch.einsum('ikl,jkl->ijl', obs_a_repr, g_repr)
 
     if len(q_action.shape) == 3:  # twin q trick